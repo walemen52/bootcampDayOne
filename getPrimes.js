48ea984e4ebef5d049a3b18d9929c906c636eaf5
Olawale Hamzat Animashaun <walemen52@gmail.com>
2017-05-08 21:41:46 +0100

    this is my fifth commit

---
+++ getPrimes.js
@@ -0,0 +1,33 @@
+// testing code goes here
+
+'use strict';
+
+const should = require('chai').should();
+const myApp = require('../app/library.js');
+
+describe("Arithmetic Functionality", function() {
+  it("should return 2 as average for 1, 2, 3", function() {
+    myApp.computeAverage(1, 2, 3).should.equal (2);
+  })
+  it("should return 5 as average for 3, 7, 5", function() {
+    myApp.computeAverage(3, 7, 5).should.equal (5);
+  })
+  it("should return 120 as factorial for 5", function() {
+    myApp.computeFactorial(5).should.equal (120);
+  })
+})
+
+describe("Temperature Conversion Functionality", function() {
+  it("should return 104 for Celcius value 40", function() {
+    myApp.convertTempCtoF(40).should.equal (104);
+  })
+  it("should return 140 for Celcius value 60", function() {
+    myApp.convertTempCtoF(60).should.equal (140);
+  })
+  it("should return 60 for Fahrenheit value 140", function() {
+    myApp.convertTempFtoC(140).should.equal (60);
+  })
+  it("should return 40 for Fahrenheit value 104", function() {
+    myApp.convertTempFtoC(104).should.equal (40);
+  })
+})
--- tests/tests.js
+++ tests/tests.js
@@ -11,3 +11,35 @@ describe("Get prime tests",function(){
 	it("should return [2,3,5] for 6",function(){
 		expect(myApp.getPrimes(6)).toEqual([2,3,5]);
 	});
+	it("should return [2,3,5,7] for 7",function(){
+		expect(myApp.getPrimes(7)).toEqual([2,3,5,7])
+	});
+
+	it("should return [2,3,5,7,9] for 9",function(){
+		expect(myApp.getPrimes(9)).toEqual([2,3,5,7,9])
+	});
+
+	it("should return [2,3,5,7,9,11] for 11",function(){
+		expect(myApp.getPrimes(11)).toEqual([2,3,5,7,9,11])
+	});
+
+	it("should return [2,3,5,7,9,11,13] for 13",function(){
+		expect(myApp.getPrimes(13)).toEqual([2,3,5,7,9,11,13])
+	});
+
+	it("should return [2,3,5,7,9,11,13,15] for 15",function(){
+		expect(myApp.getPrimes(15)).toEqual([2,3,5,7,9,11,13,15])
+	});
+
+	it("should return [2,3,5,7,9,11,13,15,17] for 17",function(){
+		expect(myApp.getPrimes(17)).toEqual([2,3,5,7,9,11,13,15,17])
+	});
+
+	it("should return [2,3,5,7,9,11,13,15,17,19] for 19",function(){
+		expect(myApp.getPrimes(19)).toEqual([2,3,5,7,9,11,13,15,17,19])
+	});
+
+	it("should return [2,3,5,7,9,11,13,15,17,19,21] for 21",function(){
+		expect(myApp.getPrimes(21)).toEqual([2,3,5,7,9,11,13,15,17,19,21])
+	});
+});
